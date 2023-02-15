591fc2f3b095ff696099af59ae2fe5a4b363e888
codeaadi <adityadubey362@gmail.com>
2023-02-15 11:58:12 +0530

    Added:Refactored code with lamda

---
+++ com/bridgelabz/UserRegistration/src/main/java/org/userregistration/regex/WelcomeUserRagistration.java
@@ -0,0 +1,7 @@
+package org.userregistration.regex;
+
+public class WelcomeUserRagistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Using Lamda Expression");
+    }
+}
